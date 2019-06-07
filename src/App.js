3dcd9719a8475071983669cdfeefb4efea65e746
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-06-06 20:33:46 -0400

    Update App.js
    add code comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Axios from 'axios';
 import apiKey from './config.js';
+
 //app componenets
 import Header from './components/Header';
 import Gallery from './components/Gallery';
@@ -12,7 +13,11 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    //bind loadHandler method to "this" keyword
     this.loadHandler = this.loadHandler.bind(this);
+
+    //initialize state for main app component
     this.state = {
       images: [],
       cats: [],
@@ -22,20 +27,25 @@ class App extends Component {
     };
   }
 
+//when the componenet first mounts these searches are performed populating the arrays in state
   componentDidMount() {
     this.performSearch("cats");
     this.performSearch("golf");
     this.performSearch("space");
 }
+
+//function that when triggered sets the loading state to true
   loadHandler(){
     this.setState({
       loading: true
     })
   }
 
+// This function performs all of the api requests and handling
   performSearch = (query) =>{
     Axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => {
+        //using a switch statement the value of "query" is checked and depending on the outcome the data is stored in the corresponding state
         switch(query){
           case "cats":
             this.setState(
@@ -75,6 +85,7 @@ class App extends Component {
   render(){
 
     return (
+      //using React-Router all the route paths are handled and state is passed into the correct components as props
       <BrowserRouter>
         <div className="container">
         <Header handler={this.loadHandler} onSearch={this.performSearch}/>
