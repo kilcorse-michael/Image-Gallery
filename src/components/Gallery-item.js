101139fce2d093604d4eecb0ddc2f88d499df261
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-06-06 20:41:20 -0400

    Update Gallery-item.js
    added code comments

--- src/components/Gallery-item.js
+++ src/components/Gallery-item.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 function GalleryItem(props){
   return(
+    //assembles a url using the information passed as props from the "Gallery" components
   <img src={`https://farm${props.farm}.staticflickr.com/${props.serverId}/${props.id}_${props.secret}_m.jpg`} alt="" />
   );
 }
